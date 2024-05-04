6364e8be59ad9a96b264c1f3f08c86c83b2c4461
ToMo <70692138+HnToMo@users.noreply.github.com>
2024-05-04 16:08:28 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,25 +2,14 @@ import * as THREE from "three";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-document.body.appendChild(renderer.domElement);
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    40,
-    window.innerWidth / window.innerHeight,
-    0.1, 100
-);
-camera.position.set(0, -50, 50);
-camera.lookAt(0, 0, 0);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-const helper = new THREE.AxesHelper(100);
-scene.add(helper);
+// const geometry = new THREE.SphereGeometry(3, 64, 64);
+// const material = new THREE.MeshStandardMaterial({
+//     color: 0x00ff83
+// });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
 
 const loader = new GLTFLoader();
 let model = null;
@@ -28,66 +17,59 @@ loader.load(
     "./tennis_ball.glb",
     function (gltf) {
         model = gltf.scene;
-        model.scale.set(8, 8, 8);
-        model.position.z = 8;
-        // model.castShadow = true;
-        // model.receiveShadow = true;
-        model.traverse(function (child) {
-            if (child.isMesh) {
-                child.castShadow = true;
-                // child.receiveShadow = true;
-            }
-        });
+        model.scale.set(3, 3, 3);
+        model.rotation.x = 10;
         scene.add(model);
     }
 );
 
-// const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
-// const box = new THREE.Mesh(
-//     boxGeometry,
-//     new THREE.MeshStandardMaterial({color: 0x333333}),
-// );
-// box.position.set(0, 0, 5);
-// box.rotation.set(0, 0, -30);
-// box.castShadow = true;
-// scene.add(box);
+const pLight = new THREE.PointLight(0xffffff, 300, 100);
+pLight.position.set(0, 10, 10);
+scene.add(pLight);
+const aLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(aLight);
 
-const planeGeometry = new THREE.PlaneGeometry(150, 150);
-const plane = new THREE.Mesh(
-    planeGeometry,
-    new THREE.MeshStandardMaterial({color: 0xaaaaaa}),
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100,
 );
-plane.receiveShadow = true;
-scene.add(plane);
+camera.position.z = 20;
+scene.add(camera);
 
-const ambientLight = new THREE.AmbientLight(0xf5deb3, 0.5);
-scene.add(ambientLight);
-const spotLight = new THREE.SpotLight(0xffffff, 1000, 100, Math.PI / 4, 1);
-spotLight.position.set(10, -10, 20);
-spotLight.castShadow = true;
-scene.add(spotLight);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 
-const clock = new THREE.Clock();
-let elapsedTime;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
 
-animate();
-onResize();
-window.addEventListener("resize", onResize);
+renderer.render(scene, camera);
 
-function animate() {
-    requestAnimationFrame(animate);
-    elapsedTime = clock.getElapsedTime();
-    if (model !== null) {
-        model.rotation.x = elapsedTime * 0.1
-        model.rotation.z = elapsedTime * 0.1
-    }
+window.addEventListener("resize", () => {
+   camera.aspect = window.innerWidth / window.innerHeight;
+   camera.updateProjectionMatrix();
+   renderer.setSize(window.innerWidth, window.innerHeight); 
+});
+
+const animate = () => {
     controls.update();
     renderer.render(scene, camera);
-}
+    window.requestAnimationFrame(animate);
+};
+animate();
 
-function onResize() {
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-}
+let mouseDown = false;
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+window.addEventListener("mousemove", (e) => {
+    if(mouseDown){
+
+    }
+});
